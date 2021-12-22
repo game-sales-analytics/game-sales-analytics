ac1bc822130fd38bec89b9c36e16a8a9c7f8d88b
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-22 10:53:05 +0330

    feat(srv): secure prometheus and swarmvisualizer web admins

--- commands/upload-swarm-files.rb
+++ commands/upload-swarm-files.rb
@@ -1,5 +1,16 @@
+require "tempfile"
+require "base64"
 require "vagrant"
 require_relative "../vms"
+
+def read_monitoring_envs(filename)
+  Hash[
+    *File.readlines(filename, chomp: true)
+      .reject { |x| x.start_with?("#") || x.strip.empty? }
+      .map { |x| x.split("=", 2) }
+      .flatten
+  ]
+end
 
 module VagrantPlugins
   module CommandUploadeManagerFiles
@@ -18,6 +29,27 @@ module VagrantPlugins
           :err => "/dev/null",
           STDOUT => STDOUT,
         )
+        envs = read_monitoring_envs File.join(File.dirname(__FILE__), "../swarm/mon/.env.caddy")
+        raw = envs["MONITORING_ADMIN_PASSWORD"]
+        encoded = Base64.strict_encode64(raw)
+        envs = envs.merge({ "MONITORING_ADMIN_PASSWORD" => encoded })
+        new_content = envs.to_a.map { |x| x.join("=") }.join("\n")
+
+        tempfile = Tempfile.new(".env.caddy")
+        tempfile.write(new_content)
+        tempfile.close
+
+        Process.wait spawn(
+          "vagrant",
+          "upload",
+          tempfile.path,
+          "/home/vagrant/swarm/mon/.env.caddy",
+          $manager_vm[:name],
+          :err => "/dev/null",
+          STDOUT => STDOUT,
+        )
+
+        tempfile.unlink
       end
     end
 
