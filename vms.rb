b11570b183fab1bb03d959bcdfe333c765a5c6d4
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-22 09:11:30 +0330

    fix(vms): reduce number of vm instances

--- vms.rb
+++ vms.rb
@@ -2,7 +2,7 @@ $manager_vm = {
   name: "manager",
   labels: [],
   memory: 4096,
-  cpus: 2,
+  cpus: 4,
   ip: "10.0.0.30",
   vb_name: "gsa-manager",
 }
@@ -10,8 +10,8 @@ $manager_vm = {
 $worker_vms = {
   dns: {
     labels: ["category=dns"],
-    memory: 1024,
-    cpus: 2,
+    memory: 2048,
+    cpus: 4,
     ip: "10.0.0.31",
   },
   monitor: {
@@ -29,7 +29,7 @@ $worker_vms = {
   dbadmins: {
     labels: ["category=dba"],
     memory: 4096,
-    cpus: 2,
+    cpus: 4,
     ip: "10.0.0.34",
   },
   cache: {
@@ -41,50 +41,32 @@ $worker_vms = {
   app1: {
     labels: ["category=app"],
     memory: 2048,
-    cpus: 2,
+    cpus: 4,
     ip: "10.0.0.36",
   },
   app2: {
     labels: ["category=app"],
     memory: 2048,
-    cpus: 2,
+    cpus: 4,
     ip: "10.0.0.37",
   },
-  app3: {
-    labels: ["category=app"],
+  dmz1: {
+    labels: ["category=dmz"],
     memory: 2048,
     cpus: 2,
     ip: "10.0.0.38",
   },
-  dmz1: {
+  dmz2: {
     labels: ["category=dmz"],
-    memory: 1024,
+    memory: 2048,
     cpus: 2,
     ip: "10.0.0.39",
   },
-  dmz2: {
-    labels: ["category=dmz"],
-    memory: 1024,
+  gateway: {
+    labels: ["category=gateway"],
+    memory: 4096,
     cpus: 2,
     ip: "10.0.0.40",
-  },
-  dmz3: {
-    labels: ["category=dmz"],
-    memory: 1024,
-    cpus: 2,
-    ip: "10.0.0.41",
-  },
-  gateway1: {
-    labels: ["category=gateway"],
-    memory: 4096,
-    cpus: 2,
-    ip: "10.0.0.42",
-  },
-  gateway2: {
-    labels: ["category=gateway"],
-    memory: 4096,
-    cpus: 2,
-    ip: "10.0.0.43",
   },
 }
   .map { |k, v| [k, v.merge({ name: "#{k}", vb_name: "gsa-#{k}" })] }.to_h
